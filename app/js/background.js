79928d2f66d158374b3f6ae83920bc6ffb6f9771
ldynia <change@me.com>
2021-11-26 19:12:02 +0100

    update

--- app/js/background.js
+++ app/js/background.js
@@ -1,6 +1,6 @@
 let color = '#3aa757';
 
-chrome.runtime.onInstalled.addListener(() => {
+chrome.runtime.onInstalled.addListener((reason) => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
